7c06ede7d9aceca155ac1d18918524f8f7bfb27c
Rafael Laidlaw <rafalaidlaw@gmail.com>
2025-02-04 13:42:32 -0500

    Added spacebar jump functionality to Dino in Player.ts class

--- src/entities/Player.ts
+++ src/entities/Player.ts
@@ -1,4 +1,6 @@
 export class Player extends Phaser.Physics.Arcade.Sprite {
+
+        cursors: Phaser.Types.Input.Keyboard.CursorKeys;
         constructor(scene: Phaser.Scene, x: number, y: number) {
           super(scene, x, y, "dino-idle");
           scene.add.existing(this);
@@ -7,6 +9,7 @@ export class Player extends Phaser.Physics.Arcade.Sprite {
           this.scene.events.on(Phaser.Scenes.Events.UPDATE, this.update, this);
         }
         init() {
+                this.cursors = this.scene.input.keyboard.createCursorKeys();
           this
             .setOrigin(0, 1)
             .setGravityY(5000)
@@ -18,7 +21,13 @@ export class Player extends Phaser.Physics.Arcade.Sprite {
             
         }
         update() {
-                console.log("Player update!");
+                const { space } = this.cursors;
+                const isSpaceJustDown = Phaser.Input.Keyboard.JustDown(space);
+                const onFloor = (this.body as Phaser.Physics.Arcade.Body).onFloor();
+                
+                if (isSpaceJustDown && onFloor) {
+                  this.setVelocityY(-1600);
+                }
               }
 
         }
